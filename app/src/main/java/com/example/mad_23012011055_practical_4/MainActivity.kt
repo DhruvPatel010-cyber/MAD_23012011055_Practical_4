9e74bc7b33fac6fd7e8d831be26125c1bba2918f
DhruvPatel010-cyber322 <dp0895653@gmail.com>
2025-09-17 10:44:39 +0530

    Initial commit

--- app/src/main/java/com/example/mad_23012011055_practical_4/MainActivity.kt
+++ app/src/main/java/com/example/mad_23012011055_practical_4/MainActivity.kt
@@ -4,14 +4,23 @@ package com.example.mad_23012011055_practical_4
 
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val textCurrentTime: TextView = findViewById(R.id.textCurrentTime)
 
